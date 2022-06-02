500d13fe500825bc6d753dc7bec4ff93e7844c60
Søren Birkemeyer <sb@webfactory.de>
2022-06-02 21:38:59 +0000

    Fix and document config

--- tasks/svgmin.js
+++ tasks/svgmin.js
@@ -1,19 +1,27 @@
 function svgmin(gulp, $, config) {
     return gulp.src(config.svgo.files, { cwd: config.webdir })
         .pipe($.svgmin({
+            full: true, // needs to be set so that the plugins list is passed directly to SVGO
             plugins: [
                 {
                     name: 'preset-default',
                     params: {
                         overrides: {
+                            // viewBox is required to resize SVGs with CSS.
+                            // @see https://github.com/svg/svgo/issues/1128
                             removeViewBox: false,
+
+                            // title and desc can be used for a11y alternative text (title with aria-labelledby)
                             removeTitle: false,
                             removeDesc: false,
+
+                            // disable task in preset-default, re-enable with custom params below
                             cleanupIDs: false,
                         },
                     }
                 },
                 {
+                    // randomize ID strings to avoid ID collisions of different inline SVGs used on the same page
                     name: 'cleanupIDs',
                     params: {
                         prefix: {
@@ -24,7 +32,6 @@ function svgmin(gulp, $, config) {
                         remove: false,
                     }
                 },
-                'removeXMLNS'
             ],
         }))
         .pipe(gulp.dest(`${config.webdir}/${config.svgo.destDir}`));
